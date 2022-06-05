666206b3bfcddb2adcf015203630eae49d9c8590
Bassam <bassamhajjxyz@gmail.com>
2022-06-05 16:15:44 +0300

    .

--- lib/layout/post/post_ad_info_layout.dart
+++ lib/layout/post/post_ad_info_layout.dart
@@ -3,7 +3,6 @@ import 'package:exservice/localization/app_localization.dart';
 import 'package:exservice/styles/app_text_style.dart';
 import 'package:exservice/utils/sizer.dart';
 import 'package:exservice/widget/application/global_widgets.dart';
-import 'package:exservice/widget/bottom_sheets/option_picker_bottom_sheet.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
